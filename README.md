00ab4e336ca7c36b6b5ca5abe2989f8c1151a393
Romain Deltour <rdeltour@gmail.com>
2013-07-25 16:19:02 +0200

    Setup Travis

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 travis-test
 ===========
 
+[![Build Status](https://travis-ci.org/rdeltour/travis-test.png?branch=master)](https://travis-ci.org/rdeltour/travis-test)
+
 Test project for Travis config
---
+++ maven_deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  mvn deploy -DskipTests -Dinvoker.skip=true --settings target/travis/settings.xml
+fi
